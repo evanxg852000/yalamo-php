7fba132cf2b58fc8d05b6751febc563b8daa0527
evanxg852000 <evanxg852000@14598e67-aabe-1442-b7d5-ab22a4115611>
2010-09-14 12:45:13 +0000

    issue of corruption

---
+++ trunk/Framework/yalamo/system/core/model.php
@@ -0,0 +1,43 @@
+<?php if ( ! defined('YPATH')) exit('Access Denied !');
+/*
+ *  MODEL IMPLEMENTATION
+ *
+ *
+ *
+ * @author Evance Soumaoro
+ */
+
+/* Class Model Definition */
+class Model {
+protected $Database;
+protected $Table;
+protected $ResultSet;
+
+public function  __construct() {
+
+}
+public function   __destruct() {
+
+}
+public function   __toString() {
+   return "Model base Class";
+}
+
+protected function Select($id="*"){
+
+}
+protected function Insert($object=array('')){
+
+}
+protected function Update($id){
+
+}
+protected function Delete($id="*"){
+
+}
+
+
+}
+
+
+
