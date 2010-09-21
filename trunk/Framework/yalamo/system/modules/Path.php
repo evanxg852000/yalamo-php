f66b1029ad56a7c3dad1c0b5fbb09910981f1050
evanxg852000 <evanxg852000@14598e67-aabe-1442-b7d5-ab22a4115611>
2010-09-21 18:09:11 +0000

    path class need to be redesigned

--- trunk/Framework/yalamo/system/modules/Path.php
+++ trunk/Framework/yalamo/system/modules/Path.php
@@ -28,26 +28,15 @@
  */
 final class Path {
     private $path;
-    private $segments;
     private $info;
     
     public function  __construct($path) {
-        $temparray=array(); 
-        if(is_array($path)){
-             $this->segments=$path; 
-             $this->path=implode(Yalamo::Ds, $path);
+        if(is_array($path)){ 
+             $this->path=implode(DS, $path);
          }
          else {
-            $this->segments=explode(Yalamo::Ds, $path);
-            //if($temparray[0]==Yalamo::Void){unset($temparray[0]);}
-            //if($temparray[count($temparray)]==Yalamo::Void){unset($temparray[count($temparray)]);}
-            foreach ($temparray as $val)
-            {
-                //[]=$val;
-            }
-            var_dump($this->segments);
-            $this->path=implode(Yalamo::Ds, $this->segments);
-        }
+            $this->path=$path;
+         }
         $this->info=pathinfo($this->path);
     }
     public function  __toString() {return "Object of Type: Path"; }
@@ -55,38 +44,29 @@ final class Path {
     public function  Append($piece){
        if(is_array($piece)){
            foreach ($piece as $val){
-               $this->segments[]=$val;
-                $this->path .=Yalamo::Ds.$val;
+                $this->path .=DS.$val;
            }
        }
        else{
-            $this->segments[]=$piece;
-            $this->path .=Yalamo::Ds.$piece;
+            $this->path .=DS.$piece;
         }
         $this->info=pathinfo($this->path);
     }
 
-    public function Segment($num=null){
-        if(is_null($num)){
-            return $this->segments;
-        }
-        else {
-            if(array_key_exists($num, $this->segments)){
-               return $this->segments[$num];
-            }
-            return null;
-        }
-        
-    }
     public function Path(){
         return $this->path;
+    }
+    public function Segements(){
+    $pathscheme="/";
+    if(!strpos($this->path,$pathscheme)){
+       $pathscheme="\\";
+    }
+        return explode($pathscheme, $this->path);
     }
     public function PathInfo(){
         return $this->info;
     }
-    public function Root(){
-        return $this->segments[0];
-    }
+
     public function Directory(){
        return dirname($this->path);
     }
