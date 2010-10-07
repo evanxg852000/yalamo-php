e023ac54c9a744ac5473d3e4c0e4343275620808
evanxg852000 <evanxg852000@14598e67-aabe-1442-b7d5-ab22a4115611>
2010-10-07 05:36:16 +0000

    weird behavior of ZipArchive class addfile method still there
    with dual mode working perfectly

--- Framework/yalamo/system/modules/Form.php
+++ Framework/yalamo/system/modules/Form.php
@@ -26,11 +26,12 @@
  * Implements abstract methods from the DBDriver class for Sqlite engine
  */
 
-final class Form {
+final class Form extends Object {
     const  Get    = "GET";
     const  Post   = "POST";
 
     private $code;
+    private $controls;
 
     public function  __construct($name, $action, $mixedata=false ,$method=Form::Post) {
         if($mixedata){
@@ -40,9 +41,118 @@ final class Form {
             $option=Yalamo::Void;
         }
         $this->code="<form name=\"$name\" action=\"$action\" method=\"$mathod\"  $option  >";
+        $this->controls=array();
     }
     public function  __toString() {return "Object of Type: Form"; }
 
+    public function Add($control){
+        if(is_callable(array($control,"Code"))){
+            $this->controls[$control->Name()]=$control;
+            return $control;
+        }
+
+    }
+    public function Remove($name){
+        if(array_key_exists($name, $this->controls)){
+            //$this->controls
+            unset($this->controls[$name]);
+        }
+
+    }
+    public function Close($dump=true){
+        foreach ($this->controls as $control){
+            $this->code .=$control->Code();
+        }
+        $this->code .="</form>";
+        if($dump){
+            echo $this->code;
+        }
+        return $this->code;
+    }
+
+}
+
+
+
+abstract class Control {
+    protected $label;
+    protected $name;
+    protected $stroption;
+
+    protected $visible;
+    Protected $enable;
+ 
+    public function __construct($label, $name,$option) {
+        $this->label=$label;
+        $this->name=$name;
+        $this->stroption=$this->formatoptions($option);
+        $this->visible=true;
+        $this->enable=true;
+    }
+    public function __toString(){return "Object of Type: Control"; }
+    private function formatoptions($options){
+        $stroption="";
+        if(!is_array($options)){
+           return;
+        }
+        foreach ($options as $name=>$value){
+            if(is_numeric($name)){continue;}
+            $stroption .=" $name=\"$value\" ";
+        }
+        return $stroption;
+    }
+
+    public function Name(){
+        return $this->name;
+    }
+    public function Visible($value){
+        if(is_bool($value)){
+            $this->visible=$value;
+        }
+    }
+    public function Enable($value){
+        if(is_bool($value)){
+            $this->visible=$value;
+        }
+
+    }
+
+    
+    public abstract function Code(){ }
+
+    
+}
+
+/*=====================================================*/
+
+class TextBox extends Control {
+    //define additional properties
+    private  $multiline;
+
+    public function  __construct($label, $name, $option) {
+        parent::__construct($label, $name,$option);
+        $this->multiline=false;
+    }  
+    public function __toString(){return "Object of Type: TextBox"; }
+
+    public function Multiline($value){
+        
+    }
+
+
+    public function  Code() {
+       return "var" ;
+    }
+}
+
+
+$t=new TextBox("","","");
+$t->Multiline=true;
+
+
+
+
+/*
 
     public function TextBox($name,$label, $multine=false,$options=null){
 
@@ -63,26 +173,4 @@ final class Form {
 
     }
 
-    public function Close($dump=true){
-        $this->code .="</form>";
-        if($dump){
-            echo $this->code;
-        }
-        return $this->code;
-    }
-
-    private function options($options){
-        $stroption="";
-        if(!is_array($options)){
-           return;
-        }
-        foreach ($options as $name=>$value){
-            if(is_numeric($name)){continue;}
-            $stroption .=" $name=\"$value\" ";
-        }
-        return $stroption;
-    }
-
-
-}
-
+   */
